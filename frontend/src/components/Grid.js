acbbadab384d1d22147339ae61559c46c5ab7363
Allan Douglas <allandouglascode@gmail.com>
2024-06-20 11:25:47 -0300

    GET woking very well

--- frontend/src/components/Grid.js
+++ frontend/src/components/Grid.js
@@ -17,15 +17,57 @@ const Table = styled.table`
 
 export const Thead = styled.thead``;
 
+export const Tbody = styled.tbody``;
+
 export const Tr = styled.tr``;
 
-const Grid = () => {
+export const Th = styled.th`
+    text-align: start;
+    border-bottom: inset;
+    padding-bottom: 5px;
+
+    @media (max-width: 500px) {
+        ${(props) => props.onlyWeb && "display: none"}
+    }
+`;
+
+export const Td = styled.td`
+    padding-top: 15px;
+    text-align: ${(props) => (props.alignCenter ? "center" : "start")};
+    width: ${(props) => props.onlyWeb && "display: none"};
+
+    @media (max-width: 500px) {
+        ${(props) => props.onlyWeb && "display: none"}
+    }
+`;
+
+const Grid = ({ users }) => {
     return (
         <Table>
             <Thead>
-                <Tr>                    
+                <Tr>
+                    <Th>Nome</Th>                    
+                    <Th>Email</Th>                    
+                    <Th onlyWeb>Fone</Th>                    
+                    <Th></Th>                    
+                    <Th></Th>                    
                 </Tr>
             </Thead>
+            <Tbody>
+                {users.map((item, i) => (
+                    <Tr key={i} >
+                        <Td width="30%">{item.nome}</Td>
+                        <Td width="30%">{item.email}</Td>
+                        <Td width="30%" onlyWeb>{item.fone}</Td>
+                        <Td alignCenter width="5%">
+                            <FaEdit />
+                        </Td>
+                        <Td>
+                            <FaTrash />
+                        </Td>
+                    </Tr>
+                ))}
+            </Tbody>
         </Table>
     )
 }
